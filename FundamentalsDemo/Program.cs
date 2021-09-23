045c0b2f6ad0cdef2491b6cf00d4b7e8ce33337d
Charles <c.friend85@yahoo.com>
2021-09-22 23:51:27 -0600

    Data Types

---
+++ FundamentalsDemo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FundamentalsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Howdy = "Hello World";
+            int Num = 7;
+            bool Fact = true;
+
+            // to declare an array you must declare its data type and you MUST declare how many indices there will be.
+            int[] numArray = new int[5] {1,2,3,4,5};
+            numArray[3] = 21; //you can change values within the array but you CANNOT change the SIZE of the array. (NO .push() or .pop())
+            // If you were to do Console.WriteLine(numArray) you would get back its TYPE not it's values.
+            // To see the values you need to loop over it.
+            for(int i = 0; i < numArray.Length; i++)
+            {
+                Console.WriteLine(numArray[i]);
+            }
+            
+            //  LISTS
+            List<string> names = new List<string>();
+            names.Add("Charles");
+            names.Add("John");
+            names.Add("Clark");
+            names.Add("Paxtonian");
+            names.Remove("Paxtonian"); //This only removes the first instance of the matching search query.
+            names.RemoveAt(2); //this will remove the value at the index specified
+            names.Insert(0, "Batman"); //insert at particular index location. 
+            
+            // iterate over list with FOREACH method making sure to declare the type of the thing you're wanting to return, 
+            // foreach loop does not give index values!!!
+            foreach(string name in names)
+            {
+                Console.WriteLine($"Your name is {name}.");
+            }
+
+            Console.WriteLine(String.Join(", ", names)); //print all names on one line separated by commas
+            Console.WriteLine(Howdy);
+            Console.WriteLine(Num);
+            Console.WriteLine(Fact);
+            Console.WriteLine(names[0]);
+
+
+            //  DICTIONARIES
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+            myDictionary.Add("One", 1);
+            myDictionary.Add("Two", 2);
+
+            Console.WriteLine($"The value at One is: {myDictionary["One"]}");
+
+
+            foreach(KeyValuePair<string, int> val in myDictionary)
+            {
+                Console.WriteLine(val.Key + " : " + val.Value);
+            }
+
+            foreach(var val in myDictionary)
+            {
+                Console.WriteLine(val.Key + " : " + val.Value);
+            }
+
+
+        }
+    }
+}
