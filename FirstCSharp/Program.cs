5198d2c4c5a3f5241adae2f586a8dabeb92d75c5
Charles <c.friend85@yahoo.com>
2021-09-22 22:16:41 -0600

    C#

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿// Importing libraries we need
+using System;
+
+// Name of our library
+namespace FirstCSharp
+{
+    class Program
+    {
+    // Scope(static means its on avail in its class) Return-type(void means no return) Name Parameter
+    // If i wanted to return a number void would become int and it would have a return statement which would NEED to be an integer
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to day one of C#!!!");
+            Console.WriteLine("New language time!!!");
+        }
+    }
+}
