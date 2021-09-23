57abac330e00f3e47d39f409f59239ec9ed12a37
Charles <c.friend85@yahoo.com>
2021-09-23 16:04:43 -0600

    Fundamentals

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 256; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for(int i = 1; i < 101; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                    Console.WriteLine(i);
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    Console.WriteLine(i);
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
