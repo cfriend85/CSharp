5e3c7591859ca63701a00a5f68b1724d543e9c9c
Charles <c.friend85@yahoo.com>
2021-09-24 14:44:37 -0600

    Puzzles

---
+++ Puzzles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomArray();
+        }
+        static int[] RandomArray()
+        {
+        int[] randArray = new int[10];
+        Random rand = new Random();
+        int min = 25;
+        int max = 0;
+        int sum = 0;
+        for(int i = 0; i < randArray.Length; i++){
+            randArray[i] = rand.Next(5, 26);
+            if(randArray[i] < min){
+                min = randArray[i];
+                sum += randArray[i];
+            }
+            else if(randArray[i] > max){
+                max = randArray[i];
+                sum += randArray[i];
+            }
+            else{
+                sum += randArray[i];
+            }
+        }
+        Console.WriteLine(string.Join(", ", randArray));
+        Console.WriteLine(min);
+        Console.WriteLine(max);
+        Console.WriteLine(sum);
+        return randArray;
+        }
+
+    }
+}
