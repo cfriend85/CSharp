2d9657de81b9483c8e4e6aa2e247db1ba2ba37c4
Charles <c.friend85@yahoo.com>
2021-10-08 17:37:05 -0600

    Validations

--- NewDojoSurvey/Models/Survey.cs
+++ NewDojoSurvey/Models/Survey.cs
@@ -3,9 +3,17 @@ namespace NewDojoSurvey.Models
 {
     public class Survey
     {
+        [Required]
+        [MinLength(2)]
         public string Name { get; set; }
+        
+        [Required]
         public string Location { get; set; }
+        
+        [Required]
         public string Language { get; set; }
+        
+        [MaxLength(20)]
         public string Comment { get; set; }
     }
 }
