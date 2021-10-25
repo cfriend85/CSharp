30bb13bc82ed29e93d6ea96e448d79c4a91506df
Charles <c.friend85@yahoo.com>
2021-10-24 20:14:40 -0600

    Finish Level 1

--- SportsORM/Controllers/HomeController.cs
+++ SportsORM/Controllers/HomeController.cs
@@ -37,6 +37,30 @@ namespace SportsORM.Controllers
             .Where(l => l.Sport.Contains("Hockey")).ToList();
             ViewBag.NotFootball = _context.Leagues
             .Where(l => !l.Sport.Contains("Football")).ToList();
+            ViewBag.Conferences = _context.Leagues
+            .Where(l => l.Name.Contains("Conference")).ToList();
+            ViewBag.Atlantic = _context.Leagues
+            .Where(l => l.Name.Contains("Atlantic")).ToList();
+            ViewBag.DallasTeams = _context.Teams
+            .Where(t => t.Location.Contains("Dallas")).ToList();
+            ViewBag.Raptors = _context.Teams
+            .Where(t => t.TeamName.Contains("Raptors")).ToList();
+            ViewBag.Cities = _context.Teams
+            .Where(t => t.Location.Contains("City")).ToList();
+            ViewBag.T = _context.Teams
+            .Where(t => t.TeamName.Contains("T")).ToList();
+            ViewBag.Alpha = _context.Teams
+            .OrderBy(t => t.Location).ToList();
+            ViewBag.Reverse = _context.Teams
+            .OrderByDescending(t => t.TeamName).ToList();
+            ViewBag.Coopers = _context.Players
+            .Where(p => p.LastName.Contains("Cooper")).ToList();
+            ViewBag.Joshuas = _context.Players
+            .Where(p => p.FirstName.Contains("Joshua")).ToList();
+            ViewBag.Specific = _context.Players
+            .Where(p => p.LastName.Contains("Cooper")).Where(p => !p.FirstName.Contains("Joshua")).ToList();
+            ViewBag.Firsts = _context.Players
+            .Where(p => p.FirstName.Contains("Alexander") || p.FirstName.Contains("Wyatt")).ToList();
             return View();
         }
 
