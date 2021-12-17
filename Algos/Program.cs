b148c1b758334b1b665fe9ff4a952a5623588367
Charles <c.friend85@yahoo.com>
2021-12-16 21:59:47 -0700

    Plus Minus Algo

--- Algos/Program.cs
+++ Algos/Program.cs
@@ -6,19 +6,20 @@ namespace Algos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("JavaScript Algos To C#!");
-            Console.WriteLine(addChar(29));
-            Console.WriteLine(adjacentProduct(new int[] {1,2,3,4,5}));
-            Console.WriteLine(angryProfessor(3, new int[] {-1,-3,4,2}));
-            Console.WriteLine(binarySearch(new int[] {1, 3, 8, 10, 12, 15, 17, 20, 22, 34, 38, 40, 50, 52, 78, 87, 90, 91, 92, 94, 200}, 52));
-            Console.WriteLine(birthdayCakeCandles(new int[] {3,2,1,3}));
-            Console.WriteLine(camelCase("saveChangesInTheEditor"));
-            Console.WriteLine(catAndMouse(2,5,4));
-            compareTriplets(new int[] {5,6,7}, new int[] {3,6,10});
-            Console.WriteLine(peaksAndValleys(8, "UDDDUDUU"));
-            Console.WriteLine(findDigits(1012));
-            Console.WriteLine(interestingPolygon(3));Console.WriteLine(isPalindrome("aabaa"));
-            Console.WriteLine(miniMaxSum(new int[] {1,2,3,4,5}));
+            // Console.WriteLine("JavaScript Algos To C#!");
+            // Console.WriteLine(addChar(29));
+            // Console.WriteLine(adjacentProduct(new int[] {1,2,3,4,5}));
+            // Console.WriteLine(angryProfessor(3, new int[] {-1,-3,4,2}));
+            // Console.WriteLine(binarySearch(new int[] {1, 3, 8, 10, 12, 15, 17, 20, 22, 34, 38, 40, 50, 52, 78, 87, 90, 91, 92, 94, 200}, 52));
+            // Console.WriteLine(birthdayCakeCandles(new int[] {3,2,1,3}));
+            // Console.WriteLine(camelCase("saveChangesInTheEditor"));
+            // Console.WriteLine(catAndMouse(2,5,4));
+            // compareTriplets(new int[] {5,6,7}, new int[] {3,6,10});
+            // Console.WriteLine(peaksAndValleys(8, "UDDDUDUU"));
+            // Console.WriteLine(findDigits(1012));
+            // Console.WriteLine(interestingPolygon(3));Console.WriteLine(isPalindrome("aabaa"));
+            // Console.WriteLine(miniMaxSum(new int[] {1,2,3,4,5}));
+            plusMinus(8, new int[] {1,2,3,-1,-2,-3,0,0});
         }
 
         public static int addChar(int n)
@@ -222,6 +223,33 @@ namespace Algos
             }
             object[] result = new object[] {sum - array[4], sum - array[0]};
             return result;
+        }
+
+        public static void plusMinus(int n, int[] array)
+        {
+            int x = array.Length;
+            int pos = 0;
+            int neg = 0;
+            int zero = 0;
+            for(int i = 0; i < x; i++)
+            {
+                if (array[i] > 0)
+                {
+                    pos += 1;
+                } else if (array[i] < 0)
+                {
+                    neg += 1;
+                } else
+                {
+                    zero += 1;
+                }
+            }
+            double posRatio = (double)pos / n;
+            double negRatio = (double)neg / n;
+            double zeroRatio = (double)zero / n;
+            Console.WriteLine(posRatio.ToString("N6"));
+            Console.WriteLine(negRatio.ToString("N6"));
+            Console.WriteLine(zeroRatio.ToString("N6"));
         }
     }
 }
