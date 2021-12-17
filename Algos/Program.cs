85df97bb2fc1811a700b0b7a03b448424b51e19e
Charles <c.friend85@yahoo.com>
2021-12-17 00:49:41 -0700

    Refactored camelCase with for each method

--- Algos/Program.cs
+++ Algos/Program.cs
@@ -6,23 +6,23 @@ namespace Algos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("JavaScript Algos To C#!");
-            Console.WriteLine(addChar(29));
-            Console.WriteLine(adjacentProduct(new int[] {1,2,3,4,5}));
-            Console.WriteLine(angryProfessor(3, new int[] {-1,-3,4,2}));
-            Console.WriteLine(binarySearch(new int[] {1, 3, 8, 10, 12, 15, 17, 20, 22, 34, 38, 40, 50, 52, 78, 87, 90, 91, 92, 94, 200}, 52));
-            Console.WriteLine(birthdayCakeCandles(new int[] {3,2,1,3}));
+            // Console.WriteLine("JavaScript Algos To C#!");
+            // Console.WriteLine(addChar(29));
+            // Console.WriteLine(adjacentProduct(new int[] {1,2,3,4,5}));
+            // Console.WriteLine(angryProfessor(3, new int[] {-1,-3,4,2}));
+            // Console.WriteLine(binarySearch(new int[] {1, 3, 8, 10, 12, 15, 17, 20, 22, 34, 38, 40, 50, 52, 78, 87, 90, 91, 92, 94, 200}, 52));
+            // Console.WriteLine(birthdayCakeCandles(new int[] {3,2,1,3}));
             Console.WriteLine(camelCase("saveChangesInTheEditor"));
-            Console.WriteLine(catAndMouse(2,5,4));
-            compareTriplets(new int[] {5,6,7}, new int[] {3,6,10});
-            Console.WriteLine(peaksAndValleys(8, "UDDDUDUU"));
-            Console.WriteLine(findDigits(1012));
-            Console.WriteLine(interestingPolygon(3));
-            Console.WriteLine(isPalindrome("aabaa"));
-            Console.WriteLine(miniMaxSum(new int[] {1,2,3,4,5}));
-            plusMinus(8, new int[] {1,2,3,-1,-2,-3,0,0});
-            Console.WriteLine(secondLargest(new int[] {5,15,77,64,31,99,45,17,0,25,1,65,148}));
-            Console.WriteLine(simpleArraySum(new int[] {1,2,3,4,5}));
+            // Console.WriteLine(catAndMouse(2,5,4));
+            // compareTriplets(new int[] {5,6,7}, new int[] {3,6,10});
+            // Console.WriteLine(peaksAndValleys(8, "UDDDUDUU"));
+            // Console.WriteLine(findDigits(1012));
+            // Console.WriteLine(interestingPolygon(3));
+            // Console.WriteLine(isPalindrome("aabaa"));
+            // Console.WriteLine(miniMaxSum(new int[] {1,2,3,4,5}));
+            // plusMinus(8, new int[] {1,2,3,-1,-2,-3,0,0});
+            // Console.WriteLine(secondLargest(new int[] {5,15,77,64,31,99,45,17,0,25,1,65,148}));
+            // Console.WriteLine(simpleArraySum(new int[] {1,2,3,4,5}));
         }
 
         public static int addChar(int n)
@@ -107,12 +107,12 @@ namespace Algos
         {
             int count = 1;
             Regex rx = new Regex("[A-Z]");
-            for(int i = 0; i < s.Length; i++)
+            foreach(char c in s)
             {
-                MatchCollection matches = rx.Matches(char.ToString(s[i]));
+                MatchCollection matches = rx.Matches(char.ToString(c));
                 foreach(Match match in matches)
                 {
-                    count += 1;
+                    count++;
                 }
             }
             return count;
